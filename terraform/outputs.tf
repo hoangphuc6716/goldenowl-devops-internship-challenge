1ee6fdec76b083246e2b79a45725ceb796dad96c
hphuc <phucdo.858903@gmail.com>
2026-07-16 12:04:19 +0700

    chinh sua noi dung file outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,7 @@
 output "ec2_public_ip" {
-  value       = aws_instance.app.public_ip
+  value       = aws_eip.app_eip.public_ip
 }
 
 output "app_url" {
-  description = "Deployment link"
-  value       = "http://${aws_instance.app.public_ip}:3000"
+  value       = "http://${aws_eip.app_eip.public_ip}:3000"
 }
