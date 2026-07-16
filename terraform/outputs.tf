36c6dc108049d5b51d52d198029ffd8e0629b63b
hphuc <phucdo.858903@gmail.com>
2026-07-16 17:44:50 +0700

    chinh sua file outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,7 @@
 output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
   value       = aws_instance.app.public_ip
 }
 
 output "app_url" {
-  description = "Deployment link"
   value       = "http://${aws_instance.app.public_ip}:3000"
 }
